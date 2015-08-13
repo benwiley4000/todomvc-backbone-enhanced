1233493841cff002cd081067c3b7f604d63a3a4b
Ben Wiley <therealbenwiley@gmail.com>
2015-08-12 22:15:12 -0400

    Added priority filter option.

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -26,6 +26,14 @@ var app = app || {};
 			return this.where({completed: false});
 		},
 
+		// Filter down the list to only priority todo items that are still not finished.
+		priority: function () {
+			return this.where({
+				priority: true,
+				completed: false
+			});
+		},
+
 		// We keep the Todos in sequential order, despite being saved by unordered
 		// GUID in the database. This generates the next order number for new items.
 		nextOrder: function () {
--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -63,9 +63,17 @@ var app = app || {};
 		},
 
 		isHidden: function () {
-			return this.model.get('completed') ?
-				app.TodoFilter === 'active' :
-				app.TodoFilter === 'completed';
+			var filter = app.TodoFilter;
+			if(filter === 'active') {
+				return this.model.get('completed');
+			}
+			if(filter === 'priority') {
+				return this.model.get('completed') || !this.model.get('priority');
+			}
+			if(filter === 'completed') {
+				return !this.model.get('completed');
+			}
+			return false;
 		},
 
 		// Toggle the `"completed"` state of the model.
